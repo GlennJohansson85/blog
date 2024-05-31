34cfd7164bbe927d9b83e4b1037b7efc322c836b
GlennJohansson85 <139962883+GlennJohansson85@users.noreply.github.com>
2024-05-31 09:29:52 +0200

    Changed back to blog due to not permitted to change the project name from blog to p4-blog

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'p4-blog',
+    'blog',
     'profiles',
 ]
 
@@ -57,7 +57,7 @@ TEMPLATES = [
         },
     },
 ]
-WSGI_APPLICATION = 'p4-blog.wsgi.application'
+WSGI_APPLICATION = 'blog.wsgi.application'
 
 
 # Database
