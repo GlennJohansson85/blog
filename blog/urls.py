073d00d059976d5f358fff86183e616030a8f08b
GlennJo <g.j@live.se>
2024-06-03 12:23:06 +0200

    Update urls.py
    delete_comment was not implemented in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.home, name='home'),
-    path('create/', views.create_post, name='create_post'),
+    path('create_post/', views.create_post, name='create_post'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('post/<int:post_id>/delete/', views.delete_post_confirmation, name='delete_post_confirmation'),
-    path('post/<int:post_id>/delete/confirm/', views.delete_post, name='delete_post'),
+    path('post/<int:post_id>/delete/', views.delete_post, name='delete_post'),
+    path('post/<int:post_id>/delete/confirm/', views.delete_post_confirmation, name='delete_post_confirmation'),
+    path('comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
 ]
