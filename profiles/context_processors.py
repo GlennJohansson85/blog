6ad431c826c6a6aad92afa79ba247d5a05ee6827
GlennJo <g.j@live.se>
2024-06-03 19:33:54 +0200

    Needed to change back context_processor due to deleting friendship functions

--- profiles/context_processors.py
+++ profiles/context_processors.py
@@ -1,13 +1,12 @@
-# profiles/context_processors.py
 from .models import Profile
 
-def profile_picture(request):
+
+def profile_context(request):
     profile_picture_url = None
     if request.user.is_authenticated:
         try:
             profile_picture_url = request.user.profile_picture.url if request.user.profile_picture else None
         except Profile.DoesNotExist:
-            # If Profile doesn't exist, handle it gracefully
             pass
 
     return {'profile_picture_url': profile_picture_url}
